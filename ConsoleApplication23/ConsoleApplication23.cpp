8855537d4b06360cff30cf3d638bcd5881851990
Шумаков Константин <shumakov.kostya@gmail.com>
2022-06-11 19:42:28 +0300

    Удалили две строки

--- ConsoleApplication23/ConsoleApplication23.cpp
+++ ConsoleApplication23/ConsoleApplication23.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	
-	cout << "Программа начала работу поток " << endl;
-	cout << "Программа начала работу поток " << endl;
+	
 	cout << "Программа начала работу поток " << endl;
 	cout << "Программа начала работу поток " << endl;
 
