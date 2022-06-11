fdbeaf7da31c81e0a60eda570db3fa1f3b308f5e
Шумаков Константин <shumakov.kostya@gmail.com>
2022-06-11 19:39:01 +0300

    Я добавил несколько строк вывода на экран для теста моего git

--- ConsoleApplication23/ConsoleApplication23.cpp
+++ ConsoleApplication23/ConsoleApplication23.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
+	
 	cout << "Программа начала работу поток " << endl;
+	cout << "Программа начала работу поток " << endl;
+	cout << "Программа начала работу поток " << endl;
+	cout << "Программа начала работу поток " << endl;
+
 	
 	return 0;
 }
